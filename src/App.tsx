a17617d5d724f8f5a46a5332d81746f961e4f381
Soumya Dubey <dubeysoumya8@gmail.com>
2022-04-07 20:20:33 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+let items=['Apple','Banana','Kiwi','Cheery','Mango'];
+let itemList=['Apple','Banana','Kiwi','Cheery','Mango'];
+items.forEach((item,index)=>{
+  return itemList.push(<li key={index}>{['Apple'];&rbrace;</li>);})
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+      <h2>This is a simple list of fruits items</h2>
+      <ul>
+        <li>Apple</li>
+        <li>Banana</li>
+        <li>Kiwi</li>
+        <li>Cheery</li>
+        <li>Mango</li>
+      </ul>
+      
+    </>
   );
 }
 
